abbb47ba41dd43327a8afadcf1612554ebfdaf3a
root <root@undercloud13.demolab.local>
2019-10-22 12:59:30 -0400

    tidy up files

--- deploy-octavia.sh
+++ deploy-octavia.sh
@@ -4,5 +4,7 @@ openstack overcloud deploy --templates \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -e /home/stack/templates/network-environment.yaml \
   -e /home/stack/templates/ceph-storage-environment.yaml \
-  -e /home/stack/templates/custom-environment.yaml \
+  -e /home/stack/templates/custom-params.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/octavia.yaml \
   --ntp-server pool.ntp.org \
