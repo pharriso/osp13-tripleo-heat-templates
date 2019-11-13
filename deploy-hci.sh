1078989046d37ba87c7ed1743eb44f411daf8490
root <root@undercloud13.demolab.local>
2019-11-13 12:57:12 -0500

    updated templates

--- deploy-hci.sh
+++ deploy-hci.sh
@@ -4,6 +4,7 @@ openstack overcloud deploy --templates \
   -e /home/stack/templates/overcloud_images.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -e /home/stack/templates/network-environment.yaml \
+  -e /home/stack/templates/hci_ports.yml\
   -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-rgw.yaml \
   -e /home/stack/templates/ceph-storage-environment.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
